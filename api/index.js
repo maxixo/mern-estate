d800895a5297d09280199f560854bd4c14bff144
maxixo <oshodiusman@yahoo.com>
2025-12-13 13:29:12 +0100

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -17,5 +18,9 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.error('❌ Failed to connect to MongoDB:', err.message);
   });
 
+  
+app.use(express.json());
+
 
   app.use("/api/user" , userRouter);
+  app.use("/api/auth" , authRouter);
