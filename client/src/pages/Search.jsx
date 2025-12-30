34793c60d9d8fafb657d4e8e1402bca5cb96e729
maxixo <oshodiusman@yahoo.com>
2025-12-30 10:55:53 +0100

    add show more button

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -19,8 +19,9 @@ const Search = () => {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore ,setShowMore ] = useState(false); 
 
-  console.log(listings)
+
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search)
@@ -45,9 +46,16 @@ const Search = () => {
 
     const fetchlisting = async () => {
          setLoading(true)
+         setShowMore(false)
          const searchQuery = urlParams.toString();
          const res = await fetch(`/api/listing/get?${searchQuery}`)
          const data = await res.json();
+         if (data.length > 8) {
+          setShowMore(true)
+         } else {
+          setShowMore(false)
+         }
+
          if (res.ok) {
            setListings(data || []);
          } else {
@@ -103,6 +111,21 @@ const Search = () => {
   const searchQuery = urlParams.toString()
   navigate({ pathname: '/search', search: `?${searchQuery}` })
  }
+
+const onShowMoreClick = async () => {
+  const numberOfListings = listings.length;
+  const startIndex = numberOfListings;
+  const urlParams = new URLSearchParams(location.search);
+  urlParams.set ('startIndex', startIndex)
+  const searchQuery = urlParams.toString();
+  const res = await fetch(`/api/listing/get?${searchQuery}`);
+  const data = await res.json();
+  if (data.length < 9) {
+    setShowMore(false);
+  }
+  setListings([...listings, ...data])
+}
+
 
   return (
     <div className='flex flex-col md:flex-row'>
@@ -202,6 +225,13 @@ const Search = () => {
                listings.map((listing) => (
                 <ListingItem  key={listing._id} listing={listing}/>
               ))
+             }
+             {
+              showMore && (
+                <button
+                 onClick={onShowMoreClick} className='text-green-700 w-full hover:underline text-center  p-3 rounded-lg uppercase
+                  hover:opacity-95'>Load more</button>
+  )
              }
           </div>
      </div>
