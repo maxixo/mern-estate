6da9f1085a829e168de9433c13bbf608c9645981
maxixo <oshodiusman@yahoo.com>
2026-01-01 00:21:20 +0100

    add about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,29 @@
 const About = () => {
   return (
-    <main>
-      <h1>About</h1>
-      <p>Learn more about this project.</p>
-    </main>
+   <div className="p-20 px-4 max-w-6xl mx-auto  ">
+    <h1 className="text-3xl font-bold mb-4 text-slate-800">Soul Estate</h1>
+    <p className="mb-4 text-slate-700 ">Soul Estate is a leading real estate agency 
+      that specializes in helping clients buy, sell and rent properties in the most desirable neigborhoods.
+      Our team of experienced agents is dedicated to 
+      providing exceptional service and making the buying and 
+      selling process as smooth as possible
+    </p>
+    <p className="mb-4 text-slate-700 ">
+      Our mission is to help our clients achieve their real estate goals by 
+      providing expert advice , personalized service and a deep 
+      understanding of the local market. Weather you are looking to buy , sell or rent
+      a property, we are here to help you every step of the way.
+    </p>
+    <p className="mb-4 text-slate-700 ">
+      Our Team of agents has a wealth of experience and knowledge in 
+      the real estate industry and we are commited to providing 
+      the highest level of service to our clients. We believe that buying 
+      or selling  a property should be an exciting and rewarding experience 
+      and we are dedicated to making that a reality for each and every one of 
+      our client. 
+    </p>
+   </div>
+
   );
 };
 
